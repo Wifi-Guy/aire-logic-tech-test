9392a74018473bec839d879f1b2cb618149497a6
Matthew Smith <08msmith6@gmail.com>
2022-02-02 17:23:00 +0000

    Created method + test for getting lyrics

--- src/apis/lyrics.py
+++ src/apis/lyrics.py
@@ -1,5 +1,9 @@
-from utilities.http_caller import wrapped_http_caller
+from src.utilities.http_caller import wrapped_http_caller
 
 
-def get_lyric_count(song_name: str):
-    raise NotImplementedError
+def get_lyric_count(artist_name: str, song_name: str, http_caller: wrapped_http_caller = wrapped_http_caller):
+    lyrics = http_caller(
+        method='GET',
+        url=f'https://api.lyrics.ovh/v1/{artist_name}/{song_name}'
+    )['lyrics']
+    return len(lyrics.split(' '))
